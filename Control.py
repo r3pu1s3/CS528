f29695916a8f9cfe7fef7f3cb516dbe29ee23a99
liyu-hz168 <liyu2004huang@gmail.com>
2026-05-06 22:15:34 -0400

    bluetooth

--- Control.py
+++ Control.py
@@ -1,27 +1,33 @@
 import CV
 import time
-import serial
-import serial.tools.list_ports
+# import serial
+# import serial.tools.list_ports
 import numpy as np
 import cv2
 from Gaze_Classifier import Gaze_Classifier
 from collections import deque
-
+import asyncio
+from bleak import BleakClient, BleakScanner
 
 # Note for MAC command to activate virtual python environment is 
 # source .venv/bin/activate 
 
-# Port auto detection
-def find_esp32_port():
-    ports = serial.tools.list_ports.comports()
-    for port in ports:
-        if 'USB' in port.description or 'CP210' in port.description or 'CH340' in port.description:
-            return port.device
-    return None
 
-port = find_esp32_port()
-print(f'Detected port: {port}')
-ser = serial.Serial(port, 115200)
+SERVICE_UUID = "0000abcd-0000-1000-8000-00805f9b34fb"
+CHAR_UUID = "00001234-0000-1000-8000-00805f9b34fb"
+DEVICE_NAME  = "ESP32-Vehicle"
+
+# # Port auto detection
+# def find_esp32_port():
+#     ports = serial.tools.list_ports.comports()
+#     for port in ports:
+#         if 'USB' in port.description or 'CP210' in port.description or 'CH340' in port.description:
+#             return port.device
+#     return None
+
+# port = find_esp32_port()
+# print(f'Detected port: {port}')
+# ser = serial.Serial(port, 115200)
 
 # Hardcoded port on mac
 # ser = serial.Serial('/dev/tty.usbserial-1140', 115200)
@@ -76,7 +82,7 @@ def config(t, deadzone, confirm_frames):
 
 
 # does live tracking
-def live_tracking(gc:Gaze_Classifier, closed_threshold = 0.15, open_threshold = 0.2, gesture_window = 3, gesture_cooldown=1.5):
+async def live_tracking(client, gc:Gaze_Classifier, closed_threshold = 0.15, open_threshold = 0.2, gesture_window = 3, gesture_cooldown=1.5):
     # init camera and detector
     cap = CV.initialize_camera(0)
     detector = CV.init_detector()
@@ -154,29 +160,32 @@ def live_tracking(gc:Gaze_Classifier, closed_threshold = 0.15, open_threshold = 
             # if activated, track gaze
             if signal_active:
                 nx, ny = CV.gaze_xy(lm, h, w)
-                
-
                 label, _ = gc.update(nx, ny)
                 # print(f"{nx}| {gc.cx} | {gc.history}")
-
         
                 if label == "RIGHT":
                     '''
                     ROBOT COMMAND CODE FOR RIGHT
                     '''
-                    ser.write(b'd')
+                    await client.write_gatt_char(CHAR_UUID, b'd', response=False)
+                    await asyncio.sleep(0.2)
+                    # ser.write(b'd')
+                    # Add delay here
                     
                 elif label == "LEFT":
                     '''             
                     ROBOT COMMAND CODE FOR LEFT
                     '''
-                    ser.write(b'a')
-                    
+                    await client.write_gatt_char(CHAR_UUID, b'a', response=False)
+                    await asyncio.sleep(0.2)
+                    # ser.write(b'a')
+                    # Add delay here
                 else:
                     '''
                     ROBOT COMMAND CODE FOR CENTER
                     '''
-                    ser.write(b'w')
+                    await client.write_gatt_char(CHAR_UUID, b'w', response=False)
+                    # ser.write(b'w')
             
                 print(label)
                 cv2.putText(frame, label, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
@@ -187,18 +196,234 @@ def live_tracking(gc:Gaze_Classifier, closed_threshold = 0.15, open_threshold = 
         if cv2.waitKey(1) & 0xFF == ord('q'):  # wait 1ms, listen for keypress
             break
 
+        # yield control to asyncio event loop between frames
+        await asyncio.sleep(0)
 
-# takes t seconds to config center for gaze
-t=4
-# a threshold to recognize right and left gaze
-deadzone = 0.008
-# number of frames to sample gaze
-frames = 3
+async def main():
+    # 4 seconds to config center for gaze 
+    # deadzone=0.008 threshold to recognize right and left gaze
+    # 3 frames to sample gaze
+    gc = config(4, 0.008, 3)
 
-gc = config(t, deadzone, frames)
+    # then connect and start tracking
+    print("Scanning for ESP32...")
+    device = await BleakScanner.find_device_by_name(DEVICE_NAME, timeout=10)
+    if device is None:
+        print("ESP32 not found. Is it powered on and advertising?")
+        return
 
-# live tracking based on coordinate stored in gc gaze classifier
-live_tracking(gc)
+    async with BleakClient(device) as client:
+        print("Connected!")
+        await live_tracking(client, gc)
+
+
+asyncio.run(main())
+
+
+
+
+# No bluetooth ver 
+# import CV
+# import time
+# import serial
+# import serial.tools.list_ports
+# import numpy as np
+# import cv2
+# from Gaze_Classifier import Gaze_Classifier
+# from collections import deque
+
+
+# # Note for MAC command to activate virtual python environment is 
+# # source .venv/bin/activate 
+
+# # Port auto detection
+# def find_esp32_port():
+#     ports = serial.tools.list_ports.comports()
+#     for port in ports:
+#         if 'USB' in port.description or 'CP210' in port.description or 'CH340' in port.description:
+#             return port.device
+#     return None
+
+# port = find_esp32_port()
+# print(f'Detected port: {port}')
+# ser = serial.Serial(port, 115200)
+
+# # Hardcoded port on mac
+# # ser = serial.Serial('/dev/tty.usbserial-1140', 115200)
+
+
+# '''
+# Compiles all the tools from CV to create UI/control loop for the user
+# '''
+
+# def menu():
+#     while (True):
+#         pass
+
+# def config(t, deadzone, confirm_frames):
+#     xs, ys = [], []
+
+#     # initialize camera, detector
+#     cap = CV.initialize_camera(0)
+
+#     detector = CV.init_detector()
+    
+#     # get time
+#     t0 = time.time()
+#     # run for 4 seconds
+#     while time.time() - t0 < t:
+#         # get and process frame, return gaze
+#         frame = CV.get_frame(cap)
+#         h,w = frame.shape[:2]
+
+#         img = CV.to_mp_img(frame)
+
+#         lm = CV.run_detector(detector, img)
+        
+#         # sample center gaze
+#         if lm:
+#             g = CV.gaze_xy(lm, h, w)
+#             xs.append(g[0]); ys.append(g[1])
+                
+#         cv2.waitKey(1) 
+#         cv2.imshow("Feed", frame)
+
+
+#     if xs and ys:
+#         cx, cy = float(np.median(xs)), float(np.median(ys))
+#         print("center:", cx, cy)
+#         # eval_js(f"setStatus('calibrated center: {cx:.3f}, {cy:.3f}')")
+#         cv2.destroyAllWindows()
+
+#         return Gaze_Classifier(cx, cy, deadzone, confirm_frames)
+#     else:
+#         print("Failed to calibrate center. Ensure webcam is active.")
+
+
+# # does live tracking
+# def live_tracking(gc:Gaze_Classifier, closed_threshold = 0.15, open_threshold = 0.2, gesture_window = 3, gesture_cooldown=1.5):
+#     # init camera and detector
+#     cap = CV.initialize_camera(0)
+#     detector = CV.init_detector()
+    
+#     # state variables
+#     blink_count = 0
+#     eye_state = "open"   # states: "open", "closed"
+#     avg_ear = 0.0
+    
+#     signal_active = False
+#     blink_times = deque()
+#     last_gesture_time = -1e9
+#     last_signal_text = "DEACTIVATED"
+#     # streams
+#     while(True):
+        
+#         # gets and process image
+#         frame = CV.get_frame(cap)
+#         img = CV.to_mp_img(frame)
+        
+#         # gets height and width
+#         h,w = frame.shape[:2]
+        
+#         # get face mesh
+#         lm = CV.run_detector(detector, img)
+        
+        
+        
+#         # if mesh  is successfully retrieved
+#         if lm:
+#             # get time
+#             now = time.monotonic()
+
+#             # gets open or closed status:
+#             left_ear = CV.eye_aspect_ratio(lm, CV.LEFT_EYE_IDX, w, h)
+#             right_ear = CV.eye_aspect_ratio(lm, CV.RIGHT_EYE_IDX, w, h)
+#             avg_ear = (left_ear + right_ear) / 2.0
+
+#             if eye_state == "open":
+#                 status_text = "Eyes open"
+#                 if avg_ear < closed_threshold:
+#                     eye_state = "closed"
+#                     # status_text = "Eyes closed"
+#             else:
+#                 status_text = "Eyes closed"
+#                 if avg_ear > open_threshold:
+#                     eye_state = "open"
+#                     blink_count += 1
+#                     # note the time the user blinked
+#                     blink_times.append(now)
+
+#                     # status_text = "Blink counted"
+            
+#             # only keep track of recent blinks
+#             while blink_times and (now - blink_times[0] > gesture_window):
+#                 blink_times.popleft()
+
+#             # Trigger only if not in cooldown
+#             if now - last_gesture_time >= gesture_cooldown:
+#                 if len(blink_times) >= 3:
+#                     signal_active = not signal_active
+#                     last_gesture_time = now
+#                     blink_times.clear()
+
+#                     if signal_active:
+#                         last_signal_text = "ACTIVATED"
+#                         print("ACTIVATED")
+#                     else:
+#                         last_signal_text = "DEACTIVATED"
+#                         print("DEACTIVATED")
+
+#             # print(blink_count)
+#             cv2.putText(frame, f"{last_signal_text}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+
+#             # if activated, track gaze
+#             if signal_active:
+#                 nx, ny = CV.gaze_xy(lm, h, w)
+                
+
+#                 label, _ = gc.update(nx, ny)
+#                 # print(f"{nx}| {gc.cx} | {gc.history}")
+
+        
+#                 if label == "RIGHT":
+#                     '''
+#                     ROBOT COMMAND CODE FOR RIGHT
+#                     '''
+#                     ser.write(b'd')
+                    
+#                 elif label == "LEFT":
+#                     '''             
+#                     ROBOT COMMAND CODE FOR LEFT
+#                     '''
+#                     ser.write(b'a')
+                    
+#                 else:
+#                     '''
+#                     ROBOT COMMAND CODE FOR CENTER
+#                     '''
+#                     ser.write(b'w')
+            
+#                 print(label)
+#                 cv2.putText(frame, label, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        
+#         # show image
+#         cv2.imshow("Gaze Direction", frame)
+        
+#         if cv2.waitKey(1) & 0xFF == ord('q'):  # wait 1ms, listen for keypress
+#             break
+
+
+# # takes t seconds to config center for gaze
+# t=4
+# # a threshold to recognize right and left gaze
+# deadzone = 0.008
+# # number of frames to sample gaze
+# frames = 3
+
+# gc = config(t, deadzone, frames)
+
+# # live tracking based on coordinate stored in gc gaze classifier
+# live_tracking(gc)
 
 
 
--- esp32-s3-vehicle-control/main/main.c
+++ esp32-s3-vehicle-control/main/main.c
@@ -8,6 +8,17 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+// For bluetooth set up 
+#include "nimble/nimble_port_freertos.h"
+#include "nimble/nimble_port.h"
+#include "nvs_flash.h"
+#include "host/ble_hs.h"
+#include "services/gap/ble_svc_gap.h"
+#include "services/gatt/ble_svc_gatt.h"
+
+#define SERVICE_UUID        0xABCD
+#define CHAR_UUID           0x1234
+
 #define INPUT_TIMEOUT_MS 200
 
 #define MOTOR_IN1_PIN           GPIO_NUM_15   // IN1 pin connected to motor driver (left)
@@ -27,15 +38,12 @@
 #define MOTOR_PWM_RES           LEDC_TIMER_10_BIT  // PWM resolution (10-bit)
 #define MAX_DUTY_CYCLE          1023  // Maximum duty cycle for 10-bit resolution (1023)
 
-// Testing, get rid of this, not needed anymore
-// int direction_for_left = 1;  // 1 for clockwise, 0 for anticlockwise  
-//                              // 1 for forward, 0 for backwards
-// int direction_for_right = 1; // 1 for anticlockwise, 0 for clockwise 
-//                              // same as direction for left, 1 forward, zero backwards
-
 // Speed control
 int duty_cycle = 700;  //1023 is max it can go, 
                        // after testing it seems that 700 is the lowest it can go while still functioning normally
+
+// Tracks the last time a command was received via BLE
+static volatile int64_t last_cmd_time = 0;
 
 void stop() {
     // set and apply via update. Set pwn to 0 for either stop or resetting purposes
@@ -56,6 +64,7 @@ void enable_motors() {
 void backward() {
     // Set spin diretion to backward
     stop();
+    vTaskDelay(pdMS_TO_TICKS(10));  // give driver time to settle
     gpio_set_level(MOTOR_IN3_PIN, 0);
     gpio_set_level(MOTOR_IN4_PIN, 1);
     gpio_set_level(MOTOR_IN1_PIN, 0);
@@ -65,6 +74,7 @@ void backward() {
 void forward() {
     // Set spin diretion to forward
     stop();
+    vTaskDelay(pdMS_TO_TICKS(10));  // give driver time to settle
     gpio_set_level(MOTOR_IN3_PIN, 1);
     gpio_set_level(MOTOR_IN4_PIN, 0);
     gpio_set_level(MOTOR_IN1_PIN, 1);
@@ -75,6 +85,7 @@ void forward() {
 void left() {
     // Move right wheel only to turn left
     stop(); 
+    vTaskDelay(pdMS_TO_TICKS(10));  // give driver time to settle
     gpio_set_level(MOTOR_IN3_PIN, 1);
     gpio_set_level(MOTOR_IN4_PIN, 0);
     gpio_set_level(MOTOR_IN1_PIN, 0);
@@ -89,6 +100,7 @@ void left() {
 void right() {
     // Move left wheel only to turn right
     stop();
+    vTaskDelay(pdMS_TO_TICKS(10));  // give driver time to settle
     gpio_set_level(MOTOR_IN3_PIN, 0);
     gpio_set_level(MOTOR_IN4_PIN, 0);
     gpio_set_level(MOTOR_IN1_PIN, 1);
@@ -99,6 +111,81 @@ void right() {
     ledc_update_duty(MOTOR_PWM_MODE, MOTOR_PWM_CHANNEL_RIGHT);
     ledc_update_duty(MOTOR_PWM_MODE, MOTOR_PWM_CHANNEL_LEFT);
 }
+
+// More bluetooth stuff 
+
+// Advertise so computer can find it
+void start_advertising(void) {
+    struct ble_hs_adv_fields fields = {0};
+    const char *name = "ESP32-Vehicle";
+    fields.name = (uint8_t *)name;
+    fields.name_len = strlen(name);
+    fields.name_is_complete = 1;
+    ble_gap_adv_set_fields(&fields);
+
+    struct ble_gap_adv_params adv_params = {0};
+    adv_params.conn_mode = BLE_GAP_CONN_MODE_UND;
+    adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN;
+    ble_gap_adv_start(BLE_OWN_ADDR_PUBLIC, NULL, BLE_HS_FOREVER, &adv_params, NULL, NULL);
+}
+
+// Called when BLE stack finishes initializing, start advertising here
+static void on_sync(void) {
+    start_advertising();  // the function from my previous message
+    printf("BLE ready, advertising as ESP32-Vehicle\n");
+}
+
+//runs the BLE host loop
+static void ble_host_task(void *param) {
+    nimble_port_run();           // blocks here, handling all BLE events internally
+    nimble_port_freertos_deinit();
+}
+
+// Watchdog: if no command received in 500ms, stop motors
+static void timeout_watchdog(void *arg) {
+    while (true) {
+        vTaskDelay(pdMS_TO_TICKS(100));  // check every 100ms
+        int64_t now = esp_timer_get_time();
+        if (last_cmd_time > 0 && (now - last_cmd_time) > 500000) { 
+            stop();
+        }
+    }
+}
+
+// Callback fires when computer write byte to serial
+static int motor_char_access(uint16_t conn_handle, uint16_t attr_handle,
+                              struct ble_gatt_access_ctxt *ctxt, void *arg) {
+    if (ctxt->op == BLE_GATT_ACCESS_OP_WRITE_CHR) {
+        last_cmd_time = esp_timer_get_time(); 
+        uint8_t cmd = ctxt->om->om_data[0];  // byte sent by computer 
+        printf("Received command: %c\n", cmd); 
+        switch (cmd) {
+            case 'w': printf("Going forward\n"); forward();  break;
+            case 'a': printf("Going left\n");  left();     break;
+            case 's': printf("Going backward\n"); backward(); break;
+            case 'd': printf("Going right\n"); right();    break;
+            default:  printf("Stopping\n");  stop();     break;
+        }
+    }
+    return 0;
+}
+
+// GATT service table
+static const struct ble_gatt_svc_def gatt_services[] = {
+    {
+        .type = BLE_GATT_SVC_TYPE_PRIMARY,
+        .uuid = BLE_UUID16_DECLARE(SERVICE_UUID),
+        .characteristics = (struct ble_gatt_chr_def[]) {
+            {
+                .uuid = BLE_UUID16_DECLARE(CHAR_UUID),
+                .access_cb = motor_char_access,
+                .flags = BLE_GATT_CHR_F_WRITE | BLE_GATT_CHR_F_WRITE_NO_RSP,
+            },
+            { 0 }  // terminator
+        },
+    },
+    { 0 }  // terminator
+};
 
 
 void app_main(void) {
@@ -151,51 +238,277 @@ void app_main(void) {
     // give hardware time to stabilize/initialize everything
     vTaskDelay(pdMS_TO_TICKS(500)); 
 
-    // Note: speed control added (speed control uses ledc)
+    // NimBLE (Bluetooth) Initialization 
+    nvs_flash_init();                   
+    nimble_port_init();
 
+    ble_svc_gap_init();                
+    ble_svc_gatt_init();
+
+    ble_gatts_count_cfg(gatt_services); 
+    ble_gatts_add_svcs(gatt_services);
+
+    ble_hs_cfg.sync_cb = on_sync;    
+
+    nimble_port_freertos_init(ble_host_task); 
+
+    // Timeout watchdog task
+    xTaskCreate(timeout_watchdog, "watchdog", 2048, NULL, 5, NULL);
+
+    // REMEMBER, BLE runs in its own task, not part of main
     while (true) {
-
         stop();
-        char key = getchar(); 
+        vTaskDelay(pdMS_TO_TICKS(1000));
 
-        // Clear buff of everything received, read most recent
-        int ch;
-        while ((ch = fgetc(stdin)) != EOF);
+        // Old code before bluetooth, REMEMBER TO DELETE LATER!!
+        // stop();
+        // char key = getchar(); 
 
-        // Debug print
-        // printf("Received: %c\n", key);
+        // // Clear buff of everything received, read most recent
+        // int ch;
+        // while ((ch = fgetc(stdin)) != EOF);
 
-        switch (key) {
-            case 'w': // Forward
-                forward();
-                break; 
+        // switch (key) {
+        //     case 'w': // Forward
+        //         forward();
+        //         break; 
 
-            case 'a': // Left
-                left();
-                break; 
+        //     case 'a': // Left
+        //         left();
+        //         break; 
 
-            case 's': // Backward 
-                backward();
-                break; 
+        //     case 's': // Backward 
+        //         backward();
+        //         break; 
 
-            case 'd': // Right
-                right();
-                break; 
+        //     case 'd': // Right
+        //         right();
+        //         break; 
 
-            default: 
-                stop();
-                break; 
+        //     default: 
+        //         stop();
+        //         break; 
             
-        }      
+        // }      
 
-        if (key == 'a' || key == 'd') {
-            // Shorter delay for more controlled turns
-            vTaskDelay(pdMS_TO_TICKS(200)); 
-        }
-        else {
-            vTaskDelay(pdMS_TO_TICKS(500)); 
-        }
+        // if (key == 'a' || key == 'd') {
+        //     // Shorter delay for more controlled turns
+        //     vTaskDelay(pdMS_TO_TICKS(200)); 
+        // }
+        // else {
+        //     vTaskDelay(pdMS_TO_TICKS(500)); 
+        // }
     }
             
 
 }
+
+
+
+
+
+
+
+// No bluetooth ver
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include "esp_log.h"
+// #include "esp_timer.h"
+// #include <driver/ledc.h>
+// #include "driver/gpio.h"  
+// #include "driver/uart.h"
+// #include "freertos/FreeRTOS.h"
+// #include "freertos/task.h"
+
+// #define INPUT_TIMEOUT_MS 200
+
+// #define MOTOR_IN1_PIN           GPIO_NUM_15   // IN1 pin connected to motor driver (left)
+// #define MOTOR_IN2_PIN          GPIO_NUM_7  // IN2 pin connected to motor driver (left)
+// #define MOTOR_ENABLE_PIN_LEFT  GPIO_NUM_8  // Enable pin for PWM control on left motor
+
+// #define MOTOR_IN3_PIN          GPIO_NUM_6  // IN3 pin connected to motor driver (right)
+// #define MOTOR_IN4_PIN          GPIO_NUM_5  // IN4 pin connected to motor driver (right)
+// #define MOTOR_ENABLE_PIN_RIGHT GPIO_NUM_4  // Same as above, for right motor 
+
+
+// #define MOTOR_PWM_FREQ          5000  // Frequency in Hz for PWM
+// #define MOTOR_PWM_CHANNEL_RIGHT LEDC_CHANNEL_0
+// #define MOTOR_PWM_CHANNEL_LEFT  LEDC_CHANNEL_1
+// #define MOTOR_PWM_MODE          LEDC_LOW_SPEED_MODE
+// #define MOTOR_PWM_TIMER         LEDC_TIMER_0
+// #define MOTOR_PWM_RES           LEDC_TIMER_10_BIT  // PWM resolution (10-bit)
+// #define MAX_DUTY_CYCLE          1023  // Maximum duty cycle for 10-bit resolution (1023)
+
+// // Testing, get rid of this, not needed anymore
+// // int direction_for_left = 1;  // 1 for clockwise, 0 for anticlockwise  
+// //                              // 1 for forward, 0 for backwards
+// // int direction_for_right = 1; // 1 for anticlockwise, 0 for clockwise 
+// //                              // same as direction for left, 1 forward, zero backwards
+
+// // Speed control
+// int duty_cycle = 700;  //1023 is max it can go, 
+//                        // after testing it seems that 700 is the lowest it can go while still functioning normally
+
+// void stop() {
+//     // set and apply via update. Set pwn to 0 for either stop or resetting purposes
+//     ledc_set_duty(MOTOR_PWM_MODE, MOTOR_PWM_CHANNEL_RIGHT, 0);  
+//     ledc_set_duty(MOTOR_PWM_MODE, MOTOR_PWM_CHANNEL_LEFT, 0);
+//     ledc_update_duty(MOTOR_PWM_MODE, MOTOR_PWM_CHANNEL_RIGHT);
+//     ledc_update_duty(MOTOR_PWM_MODE, MOTOR_PWM_CHANNEL_LEFT);
+// }
+
+// void enable_motors() {
+//     // set and apply via update
+//     ledc_set_duty(MOTOR_PWM_MODE, MOTOR_PWM_CHANNEL_RIGHT, duty_cycle);
+//     ledc_set_duty(MOTOR_PWM_MODE, MOTOR_PWM_CHANNEL_LEFT, duty_cycle);
+//     ledc_update_duty(MOTOR_PWM_MODE, MOTOR_PWM_CHANNEL_RIGHT);
+//     ledc_update_duty(MOTOR_PWM_MODE, MOTOR_PWM_CHANNEL_LEFT);
+// }
+
+// void backward() {
+//     // Set spin diretion to backward
+//     stop();
+//     gpio_set_level(MOTOR_IN3_PIN, 0);
+//     gpio_set_level(MOTOR_IN4_PIN, 1);
+//     gpio_set_level(MOTOR_IN1_PIN, 0);
+//     gpio_set_level(MOTOR_IN2_PIN, 1);
+//     enable_motors();
+// } 
+// void forward() {
+//     // Set spin diretion to forward
+//     stop();
+//     gpio_set_level(MOTOR_IN3_PIN, 1);
+//     gpio_set_level(MOTOR_IN4_PIN, 0);
+//     gpio_set_level(MOTOR_IN1_PIN, 1);
+//     gpio_set_level(MOTOR_IN2_PIN, 0);
+//     enable_motors();
+// }
+
+// void left() {
+//     // Move right wheel only to turn left
+//     stop(); 
+//     gpio_set_level(MOTOR_IN3_PIN, 1);
+//     gpio_set_level(MOTOR_IN4_PIN, 0);
+//     gpio_set_level(MOTOR_IN1_PIN, 0);
+//     gpio_set_level(MOTOR_IN2_PIN, 0);
+
+//     ledc_set_duty(MOTOR_PWM_MODE, MOTOR_PWM_CHANNEL_RIGHT, duty_cycle);  
+//     ledc_set_duty(MOTOR_PWM_MODE, MOTOR_PWM_CHANNEL_LEFT, 0);
+//     ledc_update_duty(MOTOR_PWM_MODE, MOTOR_PWM_CHANNEL_RIGHT);
+//     ledc_update_duty(MOTOR_PWM_MODE, MOTOR_PWM_CHANNEL_LEFT);
+// }
+
+// void right() {
+//     // Move left wheel only to turn right
+//     stop();
+//     gpio_set_level(MOTOR_IN3_PIN, 0);
+//     gpio_set_level(MOTOR_IN4_PIN, 0);
+//     gpio_set_level(MOTOR_IN1_PIN, 1);
+//     gpio_set_level(MOTOR_IN2_PIN, 0);
+
+//     ledc_set_duty(MOTOR_PWM_MODE, MOTOR_PWM_CHANNEL_RIGHT, 0);  
+//     ledc_set_duty(MOTOR_PWM_MODE, MOTOR_PWM_CHANNEL_LEFT, duty_cycle);
+//     ledc_update_duty(MOTOR_PWM_MODE, MOTOR_PWM_CHANNEL_RIGHT);
+//     ledc_update_duty(MOTOR_PWM_MODE, MOTOR_PWM_CHANNEL_LEFT);
+// }
+
+
+// void app_main(void) {
+//     // Configure GPIO for motor direction control
+//     esp_rom_gpio_pad_select_gpio(MOTOR_IN1_PIN);
+//     gpio_set_direction(MOTOR_IN1_PIN, GPIO_MODE_OUTPUT);
+//     esp_rom_gpio_pad_select_gpio(MOTOR_IN2_PIN);
+//     gpio_set_direction(MOTOR_IN2_PIN, GPIO_MODE_OUTPUT);
+
+//     esp_rom_gpio_pad_select_gpio(MOTOR_IN3_PIN);
+//     gpio_set_direction(MOTOR_IN3_PIN, GPIO_MODE_OUTPUT);
+//     esp_rom_gpio_pad_select_gpio(MOTOR_IN4_PIN);
+//     gpio_set_direction(MOTOR_IN4_PIN, GPIO_MODE_OUTPUT);
+
+
+//     // Configure PWM timer
+//     ledc_timer_config_t pwm_timer = {
+//         .speed_mode       = MOTOR_PWM_MODE,
+//         .duty_resolution  = MOTOR_PWM_RES,
+//         .timer_num        = MOTOR_PWM_TIMER,
+//         .freq_hz          = MOTOR_PWM_FREQ,
+//         .clk_cfg          = LEDC_AUTO_CLK
+//     };
+//     ledc_timer_config(&pwm_timer);
+
+//     // Configure PWM channel right wheel
+//     ledc_channel_config_t pwm_channel_right = {
+//         .gpio_num       = MOTOR_ENABLE_PIN_RIGHT,
+//         .speed_mode     = MOTOR_PWM_MODE,
+//         .channel        = LEDC_CHANNEL_0,
+//         .intr_type      = LEDC_INTR_DISABLE,
+//         .timer_sel      = MOTOR_PWM_TIMER,
+//         .duty           = 0,
+//         .hpoint         = 0
+//     };
+//     ledc_channel_config(&pwm_channel_right);
+
+//     // Conifgure PWM channel left wheel
+//     ledc_channel_config_t pwm_channel_left = {
+//         .gpio_num       = MOTOR_ENABLE_PIN_LEFT,
+//         .speed_mode     = MOTOR_PWM_MODE,
+//         .channel        = LEDC_CHANNEL_1,
+//         .intr_type      = LEDC_INTR_DISABLE,
+//         .timer_sel      = MOTOR_PWM_TIMER,
+//         .duty           = 0,
+//         .hpoint         = 0
+//     };
+//     ledc_channel_config(&pwm_channel_left);
+
+//     // give hardware time to stabilize/initialize everything
+//     vTaskDelay(pdMS_TO_TICKS(500)); 
+
+//     // Note: speed control added (speed control uses ledc)
+
+//     while (true) {
+
+//         stop();
+//         char key = getchar(); 
+
+//         // Clear buff of everything received, read most recent
+//         int ch;
+//         while ((ch = fgetc(stdin)) != EOF);
+
+//         // Debug print
+//         // printf("Received: %c\n", key);
+
+//         switch (key) {
+//             case 'w': // Forward
+//                 forward();
+//                 break; 
+
+//             case 'a': // Left
+//                 left();
+//                 break; 
+
+//             case 's': // Backward 
+//                 backward();
+//                 break; 
+
+//             case 'd': // Right
+//                 right();
+//                 break; 
+
+//             default: 
+//                 stop();
+//                 break; 
+            
+//         }      
+
+//         if (key == 'a' || key == 'd') {
+//             // Shorter delay for more controlled turns
+//             vTaskDelay(pdMS_TO_TICKS(200)); 
+//         }
+//         else {
+//             vTaskDelay(pdMS_TO_TICKS(500)); 
+//         }
+//     }
+            
+
+// }
